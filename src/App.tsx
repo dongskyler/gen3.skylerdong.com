e80e8463449426f0aa11982a5b8e7ec878a34ece
Skyler Dong <dongskyler@gmail.com>
2020-05-12 23:17:28 -0500

    infrastructure/init-express-and-mod-workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,130 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './android-chrome-512x512.png';
 import './scss/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <p>
-          I&apos;m currently building the 3rd generation of my personal website, with React, Sass, Node, Express, MongoDB and TypeScript.
-        </p>
-        <p>
-          I just finished setting up my server (VPS). It runs on CentOS 8 and Nginx.
-        </p>
-      </header>
-      <body>
-        <div>
-          <a
-            className="App-link"
-            href="https://gen2.skylerdong.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3>
-              Visit the 2nd generation of my website
-            </h3>
-          </a>
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <br />
           <p>
-            Established in early 2020. Built with HTML, Sass, Bootstrap 4, JQuery, vanilla PHP and MySQL.
-          </p>
-        </div>
-        <div>
-          <a
-            className="App-link"
-            href="https://gen1.skylerdong.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3>
-              Visit the 1st generation of my website
-            </h3>
-          </a>
+            I&apos;m currently building the 3rd generation of my personal website, with React, Sass, Node, Express, MongoDB and TypeScript.
+            </p>
           <p>
-            Established in 2015. Built with Google Site templates.
-          </p>
-        </div>
-      </body>
-    </div>
-  );
+            I just finished setting up my server (VPS). It runs on CentOS 8 and Nginx.
+            </p>
+        </header>
+        <body>
+          <div>
+            <a
+              className="App-link"
+              href="https://gen2.skylerdong.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                Visit the 2nd generation of my website
+                </h3>
+            </a>
+            <p>
+              Established in early 2020. Built with HTML, Sass, Bootstrap 4, JQuery, vanilla PHP and MySQL.
+              </p>
+          </div>
+          <div>
+            <a
+              className="App-link"
+              href="https://gen1.skylerdong.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                Visit the 1st generation of my website
+                </h3>
+            </a>
+            <p>
+              Established in 2015. Built with Google Site templates.
+              </p>
+          </div>
+          Render the newly fetched data inside of this.state.data :
+          <p className="App-intro">{this.state.data}</p>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
 
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <br />
+    //     <p>
+    //       I&apos;m currently building the 3rd generation of my personal website, with React, Sass, Node, Express, MongoDB and TypeScript.
+    //     </p>
+    //     <p>
+    //       I just finished setting up my server (VPS). It runs on CentOS 8 and Nginx.
+    //     </p>
+    //   </header>
+    //   <body>
+    //     <div>
+    //       <a
+    //         className="App-link"
+    //         href="https://gen2.skylerdong.com/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h3>
+    //           Visit the 2nd generation of my website
+    //         </h3>
+    //       </a>
+    //       <p>
+    //         Established in early 2020. Built with HTML, Sass, Bootstrap 4, JQuery, vanilla PHP and MySQL.
+    //       </p>
+    //     </div>
+    //     <div>
+    //       <a
+    //         className="App-link"
+    //         href="https://gen1.skylerdong.com/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h3>
+    //           Visit the 1st generation of my website
+    //         </h3>
+    //       </a>
+    //       <p>
+    //         Established in 2015. Built with Google Site templates.
+    //       </p>
+    //     </div>
+    //   </body>
+    // </div>
+//   );
+// }
 
+// export default App;
