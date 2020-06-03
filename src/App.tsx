b154bdce3d2bdd34affbeaefca8611ab40c521cf
Skyler Dong <dongskyler@gmail.com>
2020-06-03 14:31:16 -0500

    Fix Eslint but Prettier still has issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './android-chrome-512x512.png';
-import './scss/App.scss';
+import React, { Component } from "react";
+import logo from "./android-chrome-512x512.png";
+import "./scss/App.scss";
 
 class App extends Component {
   state = {
-    data: null
+    data: null,
   };
 
   componentDidMount() {
     // Call our fetch function below once the component mounts
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+      .then((res) => this.setState({ data: res.express }))
+      .catch((err) => console.log(err));
   }
   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch("/express_backend");
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message)
+      throw Error(body.message);
     }
     return body;
   };
@@ -31,11 +31,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <br />
           <p>
-            I&apos;m currently building the 3rd generation of my personal website, with React, Sass, Node, Express, MongoDB and TypeScript.
-            </p>
+            I&apos;m currently building the 3rd generation of my personal
+            website, with React, Sass, Node, Express, MongoDB and TypeScript.
+          </p>
           <p>
-            I just finished setting up my server (VPS). It runs on CentOS 8 and Nginx.
-            </p>
+            I just finished setting up my server (VPS). It runs on CentOS 8 and
+            Nginx.
+          </p>
         </header>
         <body>
           <div>
@@ -45,13 +47,12 @@ class App extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3>
-                Visit the 2nd generation of my website
-                </h3>
+              <h3>Visit the 2nd generation of my website</h3>
             </a>
             <p>
-              Established in early 2020. Built with HTML, Sass, Bootstrap 4, JQuery, vanilla PHP and MySQL.
-              </p>
+              Established in early 2020. Built with HTML, Sass, Bootstrap 4,
+              JQuery, vanilla PHP and MySQL.
+            </p>
           </div>
           <div>
             <a
@@ -60,17 +61,11 @@ class App extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3>
-                Visit the 1st generation of my website
-                </h3>
+              <h3>Visit the 1st generation of my website</h3>
             </a>
-            <p>
-              Established in 2015. Built with Google Site templates.
-              </p>
+            <p>Established in 2015. Built with Google Site templates.</p>
           </div>
-          <h3>
-            Render the newly fetched data inside of this.state.data :
-          </h3>
+          <h3>Render the newly fetched data inside of this.state.data :</h3>
           <p className="App-intro">{this.state.data}</p>
         </body>
       </div>
@@ -79,54 +74,3 @@ class App extends Component {
 }
 
 export default App;
-
-// function App() {
-//   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <br />
-    //     <p>
-    //       I&apos;m currently building the 3rd generation of my personal website, with React, Sass, Node, Express, MongoDB and TypeScript.
-    //     </p>
-    //     <p>
-    //       I just finished setting up my server (VPS). It runs on CentOS 8 and Nginx.
-    //     </p>
-    //   </header>
-    //   <body>
-    //     <div>
-    //       <a
-    //         className="App-link"
-    //         href="https://gen2.skylerdong.com/"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <h3>
-    //           Visit the 2nd generation of my website
-    //         </h3>
-    //       </a>
-    //       <p>
-    //         Established in early 2020. Built with HTML, Sass, Bootstrap 4, JQuery, vanilla PHP and MySQL.
-    //       </p>
-    //     </div>
-    //     <div>
-    //       <a
-    //         className="App-link"
-    //         href="https://gen1.skylerdong.com/"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <h3>
-    //           Visit the 1st generation of my website
-    //         </h3>
-    //       </a>
-    //       <p>
-    //         Established in 2015. Built with Google Site templates.
-    //       </p>
-    //     </div>
-    //   </body>
-    // </div>
-//   );
-// }
-
-// export default App;
