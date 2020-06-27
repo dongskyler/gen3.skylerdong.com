094efac9bfe89425423d6c5839cc7c3559d9e026
Skyler Dong <dongskyler@gmail.com>
2020-06-27 02:59:06 -0500

    Debug: Fix yarn test issue. Now test passes. (#62)
    * Debug: Fix yarn test issue. Now test passes.
    
    * Yarn test still fails
    
    * Fix ReactGA in failling jest test issue
    
    * Minor updates to dependencies
    
    * Update package.json
    
    * Add serve

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,11 @@ module.exports = {
   "settings": {
     "react": {
       "version": "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
     }
   },
   "rules": {
